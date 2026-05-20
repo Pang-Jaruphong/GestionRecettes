646aac4b08c431e2ed663a07275013c0d2b2d6b6
pang-jaruphong <you@example.com>
2026-05-20 15:44:36 +0200

    Add last document

--- frontend/js/setupPassword.js
+++ frontend/js/setupPassword.js
@@ -1,4 +1,4 @@
-
+/*
 const params = new URLSearchParams(window.location.search);
 const token = params.get("token");
 
@@ -54,3 +54,5 @@ if (passwordForm && window.location.pathname.includes('setupPassword.html')) {
         }
     });
 }
+
+ */
